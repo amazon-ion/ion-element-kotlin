915013877b3faa5319ddecd106b483a48f8c2dda
David Lurton <dlurton@users.noreply.github.com>
2021-03-25 14:00:38 -0700

    add kdoc

--- test/com/amazon/ionelement/demos/java/DemoBase.java
+++ test/com/amazon/ionelement/demos/java/DemoBase.java
@@ -5,7 +5,10 @@ import com.amazon.ionelement.api.ElementLoader;
 import com.amazon.ionelement.api.IonElement;
 import org.junit.jupiter.api.Assertions;
 
+/** Base class for Java-based unit test classes that we use to demonstrate proper use of IonElement from Java. */
 public class DemoBase extends Assertions {
+
+    /** Asserts that [value] is equivalent to the [expectedIonText]. */
     protected void assertIonEquiv(String expectedIonText, IonElement value) {
         // Load the expected ion Text
         AnyElement expectedElem = ElementLoader.loadSingleElement(expectedIonText);
