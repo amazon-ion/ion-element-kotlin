01176c858b68d095364f3855fbe5086065370cd8
David Lurton <dlurton@users.noreply.github.com>
2021-03-25 16:21:55 -0700

    Improve usage of element loader API from Java. (#51) (#52)
    * Improve usage of element loader API from Java. (#51)

---
+++ test/com/amazon/ionelement/demos/java/DemoBase.java
@@ -0,0 +1,17 @@
+package com.amazon.ionelement.demos.java;
+
+import com.amazon.ionelement.api.AnyElement;
+import com.amazon.ionelement.api.ElementLoader;
+import com.amazon.ionelement.api.IonElement;
+import org.junit.jupiter.api.Assertions;
+
+/** Base class for Java-based unit test classes that we use to demonstrate proper use of IonElement from Java. */
+public class DemoBase extends Assertions {
+
+    /** Asserts that [value] is equivalent to the [expectedIonText]. */
+    protected void assertIonEquiv(String expectedIonText, IonElement value) {
+        // Load the expected ion Text
+        AnyElement expectedElem = ElementLoader.loadSingleElement(expectedIonText);
+        assertEquals(expectedElem, value);
+    }
+}
