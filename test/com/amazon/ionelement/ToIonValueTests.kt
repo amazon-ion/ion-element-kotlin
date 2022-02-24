9237271e5d8f9a714ec16048c9094eff04e9a1fe
David Lurton <dlurton@users.noreply.github.com>
2022-02-24 09:33:17 -0800

    Make `IonElement.toIonValue` use a ValueFactory instance instead of IonSystem (#64)

---
+++ test/com/amazon/ionelement/ToIonValueTests.kt
@@ -0,0 +1,24 @@
+package com.amazon.ionelement
+
+import com.amazon.ion.system.IonSystemBuilder
+import com.amazon.ionelement.api.toIonElement
+import com.amazon.ionelement.api.toIonValue
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class ToIonValueTests {
+
+    /**
+     * This test is needed due to the way `IonElement.toIonValue(ValueFactory)` uses a temporary container to obtain
+     * an `IonWriter` implementation to which the receiver will be written.
+     */
+    @Test
+    fun `IonValue instances converted from IonElement instances can be added to IonContainer instances`() {
+        val ion = IonSystemBuilder.standard().build()
+        val ionValue = ion.singleValue("1")
+        val element = ionValue.toIonElement()
+        val ionList = ion.newList(element.toIonValue(ion))
+
+        assertEquals(ion.singleValue("[1]"), ionList);
+    }
+}
